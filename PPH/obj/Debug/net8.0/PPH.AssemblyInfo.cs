adb3dff8b228f0969fd98e4b6c26311073da9c89
Yangzm408 <Yangzm408@gmail.com>
2024-11-30 19:33:17 +0800

    miniproject

--- PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
+++ PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ed2b8aaa327e7129fefc110ca2bff08586e2f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d4e753e0c10a1633d313ff070dc5544ffe5b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
