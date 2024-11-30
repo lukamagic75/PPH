df5bda94ad74eb3a98707440a9940659e07280ce
Yangzm408 <Yangzm408@gmail.com>
2024-11-30 10:22:12 +0800

    test

--- PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
+++ PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d239c22890f7f3d292fa1f058f2bfa14c6d658b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac31a1f3d2af9a3c5e54cf401e520563221af224")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
