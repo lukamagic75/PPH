e9198c6351a70adfbade3ba55789941f9afc8aec
yichengxu <gungnir0616@gmail.com>
2024-11-30 18:55:19 +0800

    + Assets Resource

--- PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
+++ PPH/obj/Debug/net8.0/PPH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ce244a0769dada6e6f18de2418b02806a07f55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb0649df0938e8ce17d7e518df702c1f1f65dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
